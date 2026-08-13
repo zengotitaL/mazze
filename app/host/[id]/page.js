79a8b8b25ca671c11c205cc84e167ef9b8efe880
zengotitaL <255214220+zengotitaL@users.noreply.github.com>
2026-08-13 08:25:40 -0400

    Update page.js

--- app/host/[id]/page.js
+++ app/host/[id]/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { use, useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { supabase } from "../../../lib/supabase";
 import { SIZE, hasPath, makeEmptyMaze } from "../../../lib/maze";
 
